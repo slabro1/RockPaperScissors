43bc902e615af3cf2f3bb415de63d50c52f4ea6d
slabro <jaimebalhego@gmail.com>
2024-07-07 19:51:33 -0300

    to be honest, i don't know

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerChoice () {
+function getComputerChoice() {
     let n = Math.floor(Math.random() * 3);
     if (n == 0) {
         return "rock";
@@ -7,6 +7,62 @@ function getComputerChoice () {
         return "paper";
     }
     else {
-        return "scissors";
+        return "scissor";
     }
 }
+
+function getHumanChoice() {
+    let input = prompt("Choose between rock, paper and scissor: ");
+    return input;
+}
+
+
+
+
+
+function playGame () {
+    let humanScore = 0;
+    let computerScore = 0; 
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice == computerChoice) {
+            console.log("draw")
+        }
+        else if (computerChoice == "paper" && humanChoice == "rock") {
+            computerScore += 1;
+            console.log("You lose! Paper beats Rock");
+        }
+        else if (computerChoice == "rock" && humanChoice == "paper") {
+            humanScore += 1;
+            console.log("You win! Paper beats Rock");
+        }
+        else if (computerChoice == "scissor" && humanChoice == "paper") {
+            computerScore += 1;
+            console.log("You lose! Scissor beats Paper")
+        }
+        else if (computerChoice == "paper" && humanChoice == "scissor") {
+            humanScore += 1;
+            console.log("You win! Scissor beats Paper");
+        }
+        else if (computerChoice == "rock" && humanChoice == "scissor") {
+            computerScore += 1;
+            console.log("You lose! Rock beats Scissor")
+        }
+        else if (computerChoice == "scissor" && humanChoice == "rock") {
+            humanScore += 1;
+            console.log("You win! Rock beats Scissor");
+        }
+    }
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    if (humanScore > computerScore) {
+        console.log("You win the game")
+    }
+    else {
+        console.log("You lose the game")
+    }
+}
+
+playGame();
