f6209fc5dca7d3a34e2cdfbce5529303382adc5a
slabro <jaimebalhego@gmail.com>
2024-07-11 21:25:59 -0300

    i change the index and script to play a round
    i change the code for when the user press a button the round is played against the machine
    and change the index putting all the buttons inside a div and have a id for be more easy do the javascript part

--- script.js
+++ script.js
@@ -11,49 +11,47 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let input = prompt("Choose between rock, paper and scissor: ");
-    return input;
-}
+buttons.addEventListener("click", (event) => {
+    let target = event.target;
+    switch(target.id) {
+        case "rock":
+            playRound("rock", computerChoice)
+            break;
+        case "paper":
+            playRound("paper", computerChoice)
+            break;
+        case "scissor":
+            playRound("scissor", computerChoice)
+            break;
+    }
+});
 
-function playGame () {
-    let humanScore = 0;
-    let computerScore = 0; 
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice == computerChoice) {
-            console.log("draw")
-        }
-        else if (computerChoice == "paper" && humanChoice == "rock") {
-            computerScore += 1;
-            console.log("You lose! Paper beats Rock");
-        }
-        else if (computerChoice == "rock" && humanChoice == "paper") {
-            humanScore += 1;
-            console.log("You win! Paper beats Rock");
-        }
-        else if (computerChoice == "scissor" && humanChoice == "paper") {
-            computerScore += 1;
-            console.log("You lose! Scissor beats Paper")
-        }
-        else if (computerChoice == "paper" && humanChoice == "scissor") {
-            humanScore += 1;
-            console.log("You win! Scissor beats Paper");
-        }
-        else if (computerChoice == "rock" && humanChoice == "scissor") {
-            computerScore += 1;
-            console.log("You lose! Rock beats Scissor")
-        }
-        else if (computerChoice == "scissor" && humanChoice == "rock") {
-            humanScore += 1;
-            console.log("You win! Rock beats Scissor");
-        }
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log("draw")
     }
-    if (humanScore > computerScore) {
-        console.log("You win the game")
+    else if (computerChoice == "paper" && humanChoice == "rock") {
+        computerScore += 1;
+        console.log("You lose! Paper beats Rock");
     }
-    else {
-        console.log("You lose the game")
+    else if (computerChoice == "rock" && humanChoice == "paper") {
+        humanScore += 1;
+        console.log("You win! Paper beats Rock");
+    }
+    else if (computerChoice == "scissor" && humanChoice == "paper") {
+        computerScore += 1;
+        console.log("You lose! Scissor beats Paper")
+    }
+    else if (computerChoice == "paper" && humanChoice == "scissor") {
+        humanScore += 1;
+        console.log("You win! Scissor beats Paper");
+    }
+    else if (computerChoice == "rock" && humanChoice == "scissor") {
+        computerScore += 1;
+        console.log("You lose! Rock beats Scissor")
+    }
+    else if (computerChoice == "scissor" && humanChoice == "rock") {
+        humanScore += 1;
+        console.log("You win! Rock beats Scissor");
     }
 }
-
-playGame();
