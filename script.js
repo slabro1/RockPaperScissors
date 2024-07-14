2d8970a2b07e24c48bd60c22b872a783fd98ab5e
slabro <jaimebalhego@gmail.com>
2024-07-14 14:34:36 -0300

    I make all the logic of javascript and change html
    i make the javascript change to be a dom dysplay instead console, catch the buttons clicks, change the html to
    the buttons stay inside a div and make a div to show the results

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function getComputerChoice() {
 
 buttons.addEventListener("click", (event) => {
     let target = event.target;
+    computerChoice = getComputerChoice()
     switch(target.id) {
         case "rock":
             playRound("rock", computerChoice)
@@ -26,32 +27,53 @@ buttons.addEventListener("click", (event) => {
     }
 });
 
+
+let computerScore = 0
+let humanScore = 0
+let result = document.querySelector("#result")
+
+function checkout(){
+    if (computerScore >= 5) {
+        result.innerText = "You lose the game";
+        buttons.addEventListener('click',function(event) {
+            event.preventDefault();
+        });
+    }
+    else if (humanScore >= 5) {
+        result.innerText = "You win the game";
+        buttons.addEventListener('click',function(event) {
+            event.preventDefault();
+        });
+    }
+}
+
 function playRound(humanChoice, computerChoice) {
     if (humanChoice == computerChoice) {
-        console.log("draw")
+        result.innerText = "draw";
     }
     else if (computerChoice == "paper" && humanChoice == "rock") {
         computerScore += 1;
-        console.log("You lose! Paper beats Rock");
+        result.innerText = "You lose! Paper beats Rock";
     }
     else if (computerChoice == "rock" && humanChoice == "paper") {
         humanScore += 1;
-        console.log("You win! Paper beats Rock");
+        result.innerText = "You win! Paper beats Rock";
     }
     else if (computerChoice == "scissor" && humanChoice == "paper") {
         computerScore += 1;
-        console.log("You lose! Scissor beats Paper")
+        result.innerText = "You lose! Scissor beats Paper";
     }
     else if (computerChoice == "paper" && humanChoice == "scissor") {
         humanScore += 1;
-        console.log("You win! Scissor beats Paper");
+        result.innerText = "You win! Scissor beats Paper";
     }
     else if (computerChoice == "rock" && humanChoice == "scissor") {
         computerScore += 1;
-        console.log("You lose! Rock beats Scissor")
+        result.innerText = "You lose! Rock beats Scissor";
     }
     else if (computerChoice == "scissor" && humanChoice == "rock") {
         humanScore += 1;
-        console.log("You win! Rock beats Scissor");
+        result.innerText = "You win! Rock beats Scissor";
     }
+    checkout();
 }
